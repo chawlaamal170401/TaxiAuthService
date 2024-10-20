c3bc0d018ee2156e751003442a52061ae4e37822
Amal Chawla <amchawla@chegg.com>
2024-10-20 11:59:20 +0530

    Project Setup

---
+++ src/main/java/org/amal/taxiauthservice/TaxiAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package org.amal.taxiauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiAuthServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TaxiAuthServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/amal/taxiauthservice/TaxiAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amal.taxiauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiAuthServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
